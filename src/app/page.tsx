90fbf8a1e3aee8ff41ee29977c63e861eab7c563
zer <rtxghost05@gmail.com>
2025-07-30 22:26:24 +0530

    clerk-convex setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <Button>Click me</Button>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
     </div>
   );
 }
